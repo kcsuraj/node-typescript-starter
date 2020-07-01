3cd7205fea53084a27de1bc83ffc412d979e12f8
Suraj kc <isurajkc@gmail.com>
2020-07-01 23:05:06 +0545

    Add type for model variable in mongoose service

--- src/services/mongooseService.ts
+++ src/services/mongooseService.ts
@@ -1,20 +1,22 @@
+import { Model } from 'mongoose';
+
 class MongooseService {
   /**
    * Moongoose model
    */
-  model: any;
+  model: Model<any>;
   /**
    * @description Create an instance of the MongooseService class
-   * @param Model {mongoose.model} Mongoose Model to use for the instance
+   * @param {Model<any>} model Mongoose Model to use for the instance
    */
-  constructor(model: any) {
+  constructor(model: Model<any>) {
     this.model = model;
   }
 
   /**
    * @description Create a new document on the Model
    * @param body {object} Body object to create the new document with
-   * @returns {Promise} Returns the results of the query
+   * @returns {Promise} body Returns the results of the query
    */
   create(body: any) {
     return this.model.create(body);
