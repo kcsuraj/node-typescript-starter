9ab5772a0d9859dfe6bb333c850348f0fd93d634
Suraj kc <isurajkc@gmail.com>
2020-07-01 22:50:55 +0545

    Add validation with Joi validation library

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from 'express';
 import { createUser } from './controller';
+import schema from './schema';
+import validator from '../../middlewares/validator';
 
 const router: Router = Router();
 
 /**
  * POST /api/users
  */
-router.post('/', createUser);
+router.post('/', validator(schema.createUser), createUser);
 
 export default router;
